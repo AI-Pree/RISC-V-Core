be29ab7d1c47825db038f80c823cb6242c46c190
ai-pree <sexypistolcs@gmail.com>
2021-07-23 22:23:31 +1000

    added test bash file for running tests from risc-v tests

---
+++ test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+git clone https://github.com/riscv/riscv-tests
+cd riscv-tests
+git submodule update --init --recursive
+autoconf
+./configure --prefix=$RISCV/target
+make
+make install
+
